eaec0531f28ab988e507ed6227f152969ba71208
logicandgit <slack.ya@yandex.ru>
2016-10-26 12:15:45 +0300

    Fix issues codestyle

--- homework_2/heads-and-legs.py
+++ homework_2/heads-and-legs.py
@@ -2,16 +2,16 @@ import math
 
 
 def animals(heads, legs):
-    Cows = int(math.trunc(legs/4) - (heads - math.ceil(float(legs)/float(4))))
+    Cows = int(math.trunc(legs / 4) - (heads - math.ceil(float(legs) / float(4))))
     Chickens = int(heads - Cows)
-    if ((Chickens * 2 + Cows * 4) == legs) and 0 <= Chickens and 0 <= Cows:
+    if (legs == (Chickens * 2 + Cows * 4)) and Chickens >= 0 and Cows >= 0:
         return (Chickens, Cows)
     else:
         return "No solutions"
 
 if __name__ == '__main__':
     print ('--------------------')
-    # print(animals(116, 282))
-    # print(animals(25, 555))
-    # print(animals(0, 0))
+    print(animals(116, 282))
+    print(animals(25, 555))
+    print(animals(0, 0))
     print(animals(12, 25))
