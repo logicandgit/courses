40325d55244f3e236caf8a26b7e42b09f50563de
logicandgit <slack.ya@yandex.ru>
2016-10-26 12:08:09 +0300

    Fix issues codestyle

--- homework_2/points-of-reflection.py
+++ homework_2/points-of-reflection.py
@@ -1,7 +1,7 @@
 def symmetric_point(p, q):
-    # len_x = q[0]-p[0]
-    # len_y = q[1]-p[1]
-    return [2*q[0] - p[0], 2*q[1] - p[1]]
+    # len_x = q[0] - p[0]
+    # len_y = q[1] - p[1]
+    return [2 * q[0] - p[0], 2 * q[1] - p[1]]
 if __name__ == '__main__':
     print('-----------------------------------')
     print(symmetric_point([0, 0], [1, 1]))
