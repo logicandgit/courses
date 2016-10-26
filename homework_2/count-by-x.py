f37f46f4d96ed148e3ca987d350fa421ce163065
logicandgit <slack.ya@yandex.ru>
2016-10-26 13:19:56 +0300

    Fix issues codestyle

--- homework_2/count-by-x.py
+++ homework_2/count-by-x.py
@@ -5,6 +5,6 @@ def count_by(x, n):
         # temp += x
         # res.append(temp)
     # return res
-    return [x * y for y in range(1, n+1)]
+    return [x * y for y in range(1, n + 1)]
 if __name__ == '__main__':
     print(count_by(50, 5))
