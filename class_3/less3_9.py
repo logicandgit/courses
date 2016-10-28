246691d10440980ac6eb8e31c849d2d7733b8afd
logicandgit <slack.ya@yandex.ru>
2016-10-28 19:25:59 +0300

    Fix issues

--- class_3/less3_9.py
+++ class_3/less3_9.py
@@ -1,17 +1,40 @@
-def replaca_sub(string, bracket, new_substring, onlyfirst=1):
+def replace_sub(string, bracket, new_substring, onlyfirst=1):
     res = []
     enter = 1
-    for sub in string.split(bracket[0]):
-        if sub.count(bracket[-1]) > 0 and enter == 1:
-                res.append(sub.replace(sub[:sub.find(bracket[-1])], new_substring))
-                res.append(bracket[0])
+    start = 0
+    end = len(string)
+    marker = 0
+
+    for ich in range(len(string)):
+        if string[ich] == bracket[0]:
+            if marker == 0:
+                end = ich
+                res.append(string[start:end + 1])
+                start = ich
+                marker += 1
+            else:
+                marker += 1
+
+        if string[ich] == bracket[-1] and enter == 1:
+            if marker == 1:
+                res.append(new_substring)
+                res.append(bracket[-1])
+                marker -= 1
+                start = ich + 1
+                end = len(string)
+
                 if onlyfirst:
-                    enter = 0
-        else:
-            res.append(sub)
-            res.append(bracket[0])
-    return "".join(res)[:-1]
+                    break
+
+            elif marker > 0:
+                marker -= 1
+
+    if start < len(string):
+        res.append(string[start:end])
+    return "".join(res)
 
 if __name__ == "__main__":
-    print replaca_sub("asdf as {asdf ass} asd {asd}", '{\}', "qqq qqq")
-    print replaca_sub("asdf as {asdf ass} asd {asd}", '{\}', "qqq qqq", onlyfirst=0)
+    print replace_sub("asdf as {asdf ass} asd {asd}", '{}', "qqq qqq")
+    print replace_sub("asdf as {asdf {ass as}s} asd {asd}", '{}', "qqq qqq", onlyfirst=0)
+    print replace_sub("asdf }as {asdf {ass as}s} asd {asd}", '{}', "qqq qqq", onlyfirst=0)
+    print replace_sub("asdf asdf {", '{}', "qqq qqq")
