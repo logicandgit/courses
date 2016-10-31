eed141728f6b0d4c8a1344b290bf4bb24b116760
logicandgit <slack.ya@yandex.ru>
2016-10-31 22:00:54 +0300

    Fix issue "dictionary is better"

--- homework_2/get-planet-name-by-id.py
+++ homework_2/get-planet-name-by-id.py
@@ -1,8 +1,16 @@
 def get_planet_name(id):
     # This doesn't work; Fix it!
-    planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn",
-               "Uranus", "Neptune")
-    return planets[id-1]
+    planets = {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus",
+        8: "Neptune"
+    }
+    return planets[id]
 
 if __name__ == '__main__':
     print(get_planet_name(3))
