ad50620b2e15f1fd1a011930cabcfcb6cb46b349
logicandgit <slack.ya@yandex.ru>
2016-10-31 22:16:36 +0300

    Fix issue "not very intuitive, 'while' would be better"

--- homework_2/no-zeros-for-heros.py
+++ homework_2/no-zeros-for-heros.py
@@ -1,7 +1,12 @@
 def no_boring_zeros(n):
-    for i in reversed(range(len(str(n)))):
-        if not (n % 10 ** i):
-            return n // 10 ** i
+    if n:
+        while n % 10 == 0:
+            n //= 10
+        return n
+    return n
+    # for i in reversed(range(len(str(n)))):
+    #     if not (n % 10 ** i):
+    #         return n // 10 ** i
 
 if __name__ == '__main__':
-    print(no_boring_zeros(1100))
+    print(no_boring_zeros(110))
