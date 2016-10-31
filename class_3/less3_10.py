459db2696839e640f7bd1cdc7849a4308014a416
logicandgit <slack.ya@yandex.ru>
2016-10-31 21:06:55 +0300

    Fix issue "Use sorted"

--- class_3/less3_10.py
+++ class_3/less3_10.py
@@ -1,12 +1,13 @@
 def sort_words(string):
-    new_list = []
-    for word in string.split():
-        for w in new_list:
-            if len(word) < w:
-                new_list.insert(0, word)
-            new_list.append(word)
-    return new_list
+    # new_list = []
 
+    # for word in string.split():
+    #     for w in new_list:
+    #         if len(word) < w:
+    #             new_list.insert(0, word)
+    #         new_list.append(word)
+    # return new_list
+    return sorted(string.split(), key=len)
 
 if __name__ == "__main__":
     print sort_words("asdf ds asd")
