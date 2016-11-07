c95f0efa99716f720f15304c5df3dbacec261930
logicandgit <slack.ya@yandex.ru>
2016-11-07 17:12:40 +0300

    Написать фунцию декоратор, которая будет писать в лог файл сведения о вызовах функций, которые ей декорированы

---
+++ class_5/decorator_3.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*- 
+import os
+import time
+
+
+def logger(path='logs.txt', day=1):
+    def wrapper(func):
+        def replacement_function(*args, **kwargs):
+            # count_date = 86400 * day
+            count_date = 3600 * day
+            info = os.stat(path)
+            creation_time_file = info.st_ctime
+            now = time.time()
+            time_live_file = now - creation_time_file
+
+            if time_live_file > count_date:
+                os.remove(path)
+
+            with open(path, 'a') as log_file:
+                # date_time = time.asctime(time.localtime())
+                date_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+                log_file.write('Function {} is execute at {}\n'.format(func.__name__, date_time))
+            return func(*args, **kwargs)
+        return replacement_function
+    return wrapper
+
+
+@logger('a.txt')
+def summarize(x, y):
+    return x + y
+
+
+@logger('a.txt')
+def mult(x, y):
+    return x * y
+
+if __name__ == '__main__':
+    summarize(1, 2)
+    summarize(3, 4)
+    summarize(5, 6)
+    mult(7, 8)
