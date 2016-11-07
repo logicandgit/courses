ade8b3969a6476bad199f3fc775ecfffb64408b8
logicandgit <slack.ya@yandex.ru>
2016-11-07 18:15:20 +0300

    Fis issue "many memory"

--- class_3/less3_3.py
+++ class_3/less3_3.py
@@ -1,8 +1,17 @@
 def fiban(n):
-    res = [1, 1]
-    for i in range(1, n-1):
-        res.append(res[i-1] + res[i])
-    return res[n-1]
+    first = 1
+    second = 1
+    for i in range(1, n):
+        first, second = second, first + second
+    return first
+
+
+# def fiban_rec(n):  # not sure about memory
+#     if n == 1 or n == 2:
+#         return 1
+#     else:
+#         return fiban_rec(n - 1) + fiban_rec(n - 2)
 
 if __name__ == '__main__':
-    print(fiban(5))
+    print(fiban(8))
+    # print(fiban_rec(8))
