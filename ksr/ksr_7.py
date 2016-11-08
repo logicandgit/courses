2808a0f60f29c4533681b4718ac4e02efef571df
logicandgit <slack.ya@yandex.ru>
2016-11-08 22:26:08 +0300

    used zip

--- ksr/ksr_7.py
+++ ksr/ksr_7.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*- 
 def get_turn_array(array):
-    return map(lambda x, y: [x, y], array[0], array[1])
+    res = []
+    for el in zip(*array):
+        res.append(list(el))
+    return res
 
 if __name__ == '__main__':
     print get_turn_array([[1, 2, 3], [4, 5, 6]])
