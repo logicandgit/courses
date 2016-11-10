708202240af598f18ef5dff32ba2784b93627440
logicandgit <slack.ya@yandex.ru>
2016-11-10 20:04:32 +0300

    fix with better

--- ksr/ksr_5.py
+++ ksr/ksr_5.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*- 
 def is_palindrome(string):
-    reversed_chars = []
-    for char in string:
-        reversed_chars.insert(0, char)
-    reversed_string = ''.join(reversed_chars)
-    if reversed_string == string:
-        return True
-    return False
+    return string[::-1] == string
 
 if __name__ == '__main__':
     print is_palindrome('asdf')
     print is_palindrome('aaa')
     print is_palindrome('asdffdsa')
-    print is_palindrome('asdf fdsa')
+    print is_palindrome('asdf, fdsa')
     print is_palindrome('asdff fdsa')
