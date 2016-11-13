9f70694bfd0a6e86133c42dd5628e909e064da63
logicandgit <slack.ya@yandex.ru>
2016-11-13 21:41:15 +0300

    last version ksr 10

--- ksr/ksr_10.py
+++ ksr/ksr_10.py
@@ -6,31 +6,16 @@ def logger(path='logs.txt'):
     def wrapper(func):
         def replacement_function(*args, **kwargs):
             try:
-                if len(args) != 4:
-                    raise Exception('Incorrect count args')
-
-                for value in args[:-1]:
-                    if not isinstance(value, str):
-                        raise Exception('Argument №{} must be string'.format(args.index(value) + 1))
-                    if not value:
-                        raise Exception('Argument №{} cannot be empty'.format(args.index(value) + 1))
-
-                brackets = args[-2]
-                if brackets not in ALL_BRACKETS:
-                    raise Exception('Argument №{} is not bracket'.format(args.index(brackets) + 1))
-
-                replace = args[-1]
-                if not isinstance(replace, int):
-                    raise Exception('Argument №{} must be int'.format(args.index(replace) + 1))
-                if not (0 <= replace <= 1):
-                    raise Exception('Argument №{} must be have value 0 or 1'.format(args.index(replace) + 1))
-
+                with open(path, 'a') as log_file:
+                    log_file.write(str(args) + '\n')
+                    value_function = func(*args, **kwargs)
+                    log_file.write(value_function + '\n')
             except Exception, err:
                 with open(path, 'a') as log_file:
-                    log_file.write(str(type(err)))
-                    log_file.write(err.message)
+                    log_file.write(str(type(err)) + '\n')
+                    log_file.write(err.message + '\n')
             else:
-                return func(*args, **kwargs)
+                return value_function
         return replacement_function
     return wrapper
 
@@ -71,7 +56,7 @@ def replace_sub(string, new_substring, bracket, replace_all):
 
 
 if __name__ == '__main__':
-    print replace_sub('asdf as {asdf ass} asd {asd}', 'q', '{}', 0)
+    print replace_sub('asdf as {asdf ass} asd {asd}', 'q', 0)
     print replace_sub('asdf as {asdf {ass as}s} asd {asd}', 'qqq qqq', '{}', 1)
     print replace_sub('asdf }as {asdf {ass as}s} asd {asd}', 'qqq qqq', '{}', 1)
     print replace_sub('asdf asdf {', 'qqq qqq', '{}', 0)
