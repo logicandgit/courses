403860a4d86eff01756d9317363f9370d934b3d2
logicandgit <slack.ya@yandex.ru>
2016-11-14 23:09:12 +0300

    Last version Not working in 2.7.6

--- class_8/linked-lists-get-nth-node.py
+++ class_8/linked-lists-get-nth-node.py
@@ -22,31 +22,25 @@ def build_one_two_three():
 
 def get_nth(node, index):
     try:
-        def length(node):
-            res = 0
-            while node:
-                res += 1
-                node = node.next
-            return res
-
-        if index >= length(node) or index < 0:
-            raise Exception('Invalid index value should throw error.')
-
-        if not node:
+        if node:
+            temp_counter = 0
+            if index >= 0:
+                while node:
+                    if temp_counter == index:
+                        return node
+                    temp_counter += 1
+                    node = node.next
+                raise Exception('Invalid index value should throw error.')
+            else:
+                raise Exception('Invalid index value should throw error.')
+        else:
             raise Exception('None linked list should throw error.')
 
-        temp_counter = 0
-        while node:
-            if temp_counter == index:
-                return node
-            temp_counter += 1
-            node = node.next
-        return node
 
     except Exception:
         return Exception.message
 
 if __name__ == '__main__':
     list_node = build_one_two_three()
-    print(get_nth(list_node, 0).data)
-    print(get_nth(list_node, 1).data)
+    print(get_nth(list_node, -1))
+    # print(get_nth(list_node, 1).data)
