c8bf749562ab7e666b1d51ea9c8decc29e2b8f80
logicandgit <slack.ya@yandex.ru>
2016-11-16 23:02:39 +0300

    Implement an InsertNth() function which can insert a new node at any index within a list.
    InsertNth() is a more general version of the Push() function that we implemented in the first kata listed below. Given a list, an index 'n' in the range 0..length, and a data element, add a new node to the list so that it has the given index. InsertNth() should return the head of the list.

---
+++ class_8/linked-lists-insert-nth-node.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*- 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def push(head, data):
+    new_node = Node(data)
+    new_node.next = head
+    return new_node
+
+
+def build_one_two_three():
+    third = push(None, 3)
+    second = push(third, 2)
+    head = push(second, 1)
+
+    return head
+
+
+def insert_nth(head, index, data):
+    try:
+        old_node = head
+        if index:
+            index -= 1
+            while head:
+                if not index:
+                    new_node = Node(data)
+                    new_node.next = head.next
+                    head.next = new_node
+                    return old_node
+                index -= 1
+                # old_node = head
+                head = head.next
+            raise Exception('Invalid index value should raise error.')
+        if not index:
+            new_node = Node(data)
+            new_node.next = head
+            return new_node
+    except Exception:
+        return Exception.message
+
+if __name__ == '__main__':
+    list_node = build_one_two_three()
+    # print(insert_nth(build_one_two_three(), 1, 23).data)
+    print(insert_nth(build_one_two_three(), 1, 23).next.next.data)
+    # print(insert_nth(build_one_two_three(), 3, 23).data)
+    # print(insert_nth(None, 0, 12).data)
+    # print(insert_nth(None, 0, 12).next)
