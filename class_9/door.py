4e866cd9947ce52c7e5caff19bface73d1608a17
logicandgit <slack.ya@yandex.ru>
2016-11-20 21:03:46 +0300

    work in class

--- class_9/door.py
+++ class_9/door.py
@@ -6,8 +6,40 @@ class Door(object):
         self.number = number
         self.state = state
 
+    @classmethod
+    def knock(cls):
+        print('Knock!Knock!!')
+
     def open(self):
         self.state = 'open'
 
     def close(self):
         self.state = 'close'
+
+
+class SecurityDoor(Door):
+    color = 'gray'
+    locked = True
+
+    def open(self):
+        if not self.locked:
+            print('Opening')
+            super(SecurityDoor, self).open()
+
+
+class CompositeDoor(object):
+    # color = 'composite'
+    locked = True
+
+    def __init__(self, number, state):
+        self.door = Door(number, state)
+
+    def open(self):
+        if not self.locked:
+            self.door.open()
+
+    def close(self):
+        self.door.close()
+
+    def __getattr__(self, attr):
+        return getattr(self.door, attr)
