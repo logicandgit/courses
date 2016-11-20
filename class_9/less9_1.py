8c6990848ce968036a7eca3e28e2705ef1433048
logicandgit <slack.ya@yandex.ru>
2016-11-20 21:05:34 +0300

    Классы Recatangular, Square, Triangle, Circle переопределяют метод __str__ (или to_string) так чтобы вызывался метод базового класса и в строку добавлялась информация о типе и атрибутах объекта.

--- class_9/less9_1.py
+++ class_9/less9_1.py
@@ -2,32 +2,61 @@
 import math
 
 
-class Rectangular:
+class Shape(object):
 
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
+    def __init__(self, elem):
+        self.elem = elem
 
     def get_area(self):
-        return self.length * self.width
+        return 0
 
     def get_perimeter(self):
-        return 2 * sum(self.length, self.width)
+        return 0
+
+    def __str__(self):
+        return 'I am shape'
+
+
+class Rectangular(Shape):
+
+    def __init__(self, length, width):
+        super(Rectangular, self).__init__(length)
+        self.width = width
+
+    def __str__(self):
+        return '{} of type {} with length {} and width {}'.format(
+            super(Rectangular, self).__str__(),
+            self.__class__.__name__,
+            self.elem, self.width
+        )
+
+    def get_area(self):
+        return self.elem * self.width
+
+    def get_perimeter(self):
+        return 2 * sum(self.elem, self.width)
 
     def get_location(self):
         pass
 
 
-class Circle:
+class Circle(Shape):
 
     def __init__(self, radius):
-        self.radius = radius
+        super(Circle, self).__init__(radius)
+
+    def __str__(self):
+        return '{} of type {} with radius {}'.format(
+            super(Circle, self).__str__(),
+            self.__class__.__name__,
+            self.elem
+        )
 
     def get_area(self):
-        return math.pi * self.radius ** 2
+        return math.pi * self.elem ** 2
 
     def get_perimeter(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.elem
 
     def get_location(self):
         pass
@@ -39,24 +68,36 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return 'I am point {}'.format(
+            tuple(self.__dict__.values())
+        )
+
     def get_distance_to(self, point):
         return math.sqrt((self.x - point.x) ** 2 + (self.y - point.y) ** 2)
 
 
-class Triangle:
+class Triangle(Shape):
 
     def __init__(self, point_one, point_two, point_three):
-        self.point_one = point_one
+        super(Triangle, self).__init__(point_one)
         self.point_two = point_two
         self.point_three = point_three
 
+    def __str__(self):
+        return '{} of type {} with points {}, {}, {}'.format(
+            super(Triangle, self).__str__(),
+            self.__class__.__name__,
+            *map(lambda x: tuple(x.__dict__.values()), self.__dict__.values())
+        )
+
     def get_area(self):
-        a, b, c = self._get_size(self.point_one, self.point_two, self.point_three)
+        a, b, c = self._get_size(self.elem, self.point_two, self.point_three)
         p = (a + b + c) / 2
         return math.sqrt(p * (p - a) * (p - b) * (p - c))
 
     def get_perimeter(self):
-        return sum(self._get_size(self.point_one, self.point_two, self.point_three))
+        return sum(self._get_size(self.elem, self.point_two, self.point_three))
 
     def _get_size(self, point1, point2, point3):
         a = point1.get_distance_to(point2)
@@ -65,29 +106,39 @@ class Triangle:
         return a, b, c
 
 
-class Square:
+class Square(Shape):
 
     def __init__(self, side):
-        self.side = side
+        super(Square, self).__init__(side)
+
+    def __str__(self):
+        return '{} of type {} with side {}'.format(
+            super(Square, self).__str__(),
+            self.__class__.__name__,
+            self.elem
+        )
 
     def get_area(self):
-        return self.side ** 2
+        return self.elem ** 2
 
     def get_perimeter(self):
-        return self.side * 4
+        return self.elem * 4
 
     def get_location(self):
         pass
 
 if __name__ == '__main__':
-    # rect = Rectangular(1, 2)
+    rect = Rectangular(1, 2)
+    print str(rect)
+    point = Point(1, 2)
+    print str(point)
     # print rect
     # print rect.get_area()
     # print rect.get_perimeter()
-
     p1 = Point(0, 0)
     p2 = Point(2, 0)
     p3 = Point(2, 2)
     triangele = Triangle(p1, p2, p3)
-    print triangele.get_area()
-    print triangele.get_perimeter()
+    print str(triangele)
+    # print triangele.get_area()
+    # print triangele.get_perimeter()
