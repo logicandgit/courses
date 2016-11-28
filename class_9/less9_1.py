760ada921f2048ce8839a0f189e38eb86b854297
logicandgit <slack.ya@yandex.ru>
2016-11-28 15:47:21 +0300

    work in class

--- class_9/less9_1.py
+++ class_9/less9_1.py
@@ -13,6 +13,22 @@ class Shape(object):
     def get_perimeter(self):
         return 0
 
+    def __cmp__(self, fig2):
+        if self.get_area() == fig2.get_area():
+            return 0
+        elif self.get_area() < fig2.get_area():
+            return -1
+        else:
+            return 1
+        # return cmp(self.get_area(), fig2.get_area())
+
+    @staticmethod
+    def get_max_shape(figures):
+        max_fig = figures[0]
+        for fig in figures[1:]:
+            max_fig = fig if fig.get_area() > max_fig.get_area() else max_fig
+        return max_fig
+
     def __str__(self):
         return 'I am shape'
 
@@ -24,10 +40,11 @@ class Rectangular(Shape):
         self.width = width
 
     def __str__(self):
-        return '{} of type {} with length {} and width {}'.format(
+        return '{} of type {} with length {} and width {}, area: {}'.format(
             super(Rectangular, self).__str__(),
             self.__class__.__name__,
-            self.elem, self.width
+            self.elem, self.width,
+            self.get_area
         )
 
     def get_area(self):
@@ -46,10 +63,11 @@ class Circle(Shape):
         super(Circle, self).__init__(radius)
 
     def __str__(self):
-        return '{} of type {} with radius {}'.format(
+        return '{} of type {} with radius {}, area: {}'.format(
             super(Circle, self).__str__(),
             self.__class__.__name__,
-            self.elem
+            self.elem,
+            self.get_area()
         )
 
     def get_area(self):
@@ -85,25 +103,39 @@ class Triangle(Shape):
         self.point_three = point_three
 
     def __str__(self):
-        return '{} of type {} with points {}, {}, {}'.format(
+        return '{} of type {} with area: {} and point {}, {}, {}'.format(
             super(Triangle, self).__str__(),
             self.__class__.__name__,
+            self.get_area(),
             *map(lambda x: tuple(x.__dict__.values()), self.__dict__.values())
         )
 
     def get_area(self):
-        a, b, c = self._get_size(self.elem, self.point_two, self.point_three)
-        p = (a + b + c) / 2
-        return math.sqrt(p * (p - a) * (p - b) * (p - c))
+        # a, b, c = self._get_size(self.elem, self.point_two, self.point_three)
+        p = (self.ab + self.bc + self.ac) / 2
+        return math.sqrt(p * (p - self.ab) * (p - self.bc) * (p - self.ac))
 
     def get_perimeter(self):
-        return sum(self._get_size(self.elem, self.point_two, self.point_three))
+        # return sum(self._get_size(self.elem, self.point_two, self.point_three))
+        return self.ab + self.bc + self.ac
 
-    def _get_size(self, point1, point2, point3):
-        a = point1.get_distance_to(point2)
-        b = point2.get_distance_to(point3)
-        c = point3.get_distance_to(point1)
-        return a, b, c
+    @property
+    def ab(self):
+        return self.elem.get_distance_to(self.point_two)
+
+    @property
+    def bc(self):
+        return self.point_two.get_distance_to(self.point_three)
+
+    @property
+    def ac(self):
+        return self.point_three.get_distance_to(self.elem)
+
+    # def _get_size(self, point1, point2, point3):
+    #     a = point1.get_distance_to(point2)
+    #     b = point2.get_distance_to(point3)
+    #     c = point3.get_distance_to(point1)
+    #     return a, b, c
 
 
 class Square(Shape):
@@ -112,10 +144,11 @@ class Square(Shape):
         super(Square, self).__init__(side)
 
     def __str__(self):
-        return '{} of type {} with side {}'.format(
+        return '{} of type {} with side {} and area: {}'.format(
             super(Square, self).__str__(),
             self.__class__.__name__,
-            self.elem
+            self.elem,
+            self.get_area()
         )
 
     def get_area(self):
@@ -128,19 +161,38 @@ class Square(Shape):
         pass
 
 if __name__ == '__main__':
-    rect = Rectangular(1, 2)
-    print str(rect)
-    point = Point(1, 2)
-    print str(point)
+    # rect = Rectangular(1, 2)
+    # print str(rect)
+    # point = Point(1, 2)
+    # print str(point)
     # print rect
     # print rect.get_area()
     # print rect.get_perimeter()
     p1 = Point(0, 0)
-    p2 = Point(2, 0)
-    p3 = Point(2, 2)
+    p2 = Point(4, 0)
+    p3 = Point(4, 4)
     triangele = Triangle(p1, p2, p3)
+    # print type(triangele.ab)
+    # print
+    # print triangele.get_perimeter()
+    # triangele.elem = Point(1, 1)
+    # print triangele.get_perimeter()
+    # triangele.ab = 1
+    # print int(2).__cmp__(1)
+    # print str(triangele)
+    square = Square(2)
+    cirle = Circle(2)
     print str(triangele)
-    square = Square(4)
     print str(square)
+    print str(cirle)
+    print square > triangele
+    print square < triangele
+    print cmp(square, triangele)
+    print cmp(triangele, square)
+    print square > cirle
+
+    print str(Shape.get_max_shape([square, cirle, triangele]))
+    # print cmp(square, triangele)
+    # print str(square)
     # print triangele.get_area()
     # print triangele.get_perimeter()
