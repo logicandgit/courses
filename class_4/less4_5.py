7cab3c47b4fc84f347d58c87995baf6f48cf7835
logicandgit <slack.ya@yandex.ru>
2016-11-28 16:06:12 +0300

    Fix issue "орфографические ошибки (название метода), метод должен возвращать boolean."

--- class_4/less4_5.py
+++ class_4/less4_5.py
@@ -1,11 +1,11 @@
-def unpuck_array(array, res):
+def unpack_array(array, res):
     """Unpack the arbitrary nested array and saves all value into the list
     :array: array with arbitrary nested
     :res: list will be saved elements array
     """
     for i in range(len(array)):
         if type(array[i]) == type(array):
-            unpuck_array(array[i], res)
+            unpack_array(array[i], res)
         else:
             res.append(array[i])
 
@@ -18,11 +18,11 @@ def check_uniqueness_array(array):
 
     """
     res = []
-    unpuck_array(array, res)
+    unpack_array(array, res)
 
     if len(set(res)) == len(res):
-        return "Unique"
-    return "Not unique"
+        return True
+    return False
 
 
 if __name__ == "__main__":
