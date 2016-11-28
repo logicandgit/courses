08f0c05b78ca993b19340829ebcbd48328746150
logicandgit <slack.ya@yandex.ru>
2016-11-28 15:53:10 +0300

    Fix issue "неудачные примеры именований переменных res и _res, summa"

--- class_3/less3_4.py
+++ class_3/less3_4.py
@@ -1,12 +1,12 @@
 def sum_mul(n):
-    summa = 0
-    mult = 1
-    _res = n
-    while _res > 0:
-        _res, res = divmod(_res, 10)
-        summa += res
-        mult *= res
-    return summa, mult
+    sum_numbers = 0
+    multiplication_numbers = 1
+    temp_value = n
+    while temp_value > 0:
+        temp_value, number = divmod(temp_value, 10)
+        sum_numbers += number
+        multiplication_numbers *= number
+    return sum_numbers, multiplication_numbers
 
 if __name__ == '__main__':
     print(sum_mul(59))
